06943f73067c1788d38a537fd940116b44da80fa
Sydney Blom <sydneyblom@#gmail.com>
2020-02-19 17:13:45 -0600

    RPS and Knapsack done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,23 @@
 
 import sys
 
+options = [["rock"], ["paper"], ["scissors"]]
+
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    if n == 1:
+        return options
+    output = []
+
+    listA = rock_paper_scissors(n - 1)
+    for subList in listA:
+        for play in options:
+            newPlay = subList + play
+            output.append(newPlay)
+    return output
+
+  
 
 
 if __name__ == "__main__":
